1fccf38270943d94251df7f75930e165a3b4fc25
KevinHeidema <45132144+canadiancode@users.noreply.github.com>
2023-02-04 22:37:05 -0800

    The basics of changing the asset and timeframe

--- src/script.js
+++ src/script.js
@@ -1,47 +1,105 @@
     // Bitcoin data from an API
 
-//variables
-const getDataButton = document.querySelector('.getDataButton');
-const bitcoinPriceURL = 'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=365&interval=daily';
-
-//async await code
-async function fetchData() {
-
-    // Get the dataset from CoinGecko API
-    let response = await fetch(bitcoinPriceURL);
-    let bitcoinData = await response.json();
-    let bitcoinPrice = await bitcoinData.prices;
-
-    // extraction of the desired data from dataset (time and price)
-    for (let i = 0; i < bitcoinPrice.length; i++) {
-        let timeframe = bitcoinPrice[i][0];
-        let price = Math.round(bitcoinPrice[i][1]);
-
-        chartTimeframe.push(timeframe);
-        chartPrice.push(price);
-    };
-
-    // console price and time values
-    console.log(chartTimeframe);
-    console.log(chartPrice);
-
-    // update the chart with new data
-    displayedChart.data.labels = chartTimeframe;
-    displayedChart.data.datasets.forEach(dataset => {
-        dataset.data = chartPrice;
-    });
-    
-    displayedChart.update();
-
-};
-
-
+// arrays for the price and timeframe
 let chartTimeframe = [];
 let chartPrice = [];
-getDataButton.addEventListener('click', fetchData);
+
+// variables for the data
+let selectedAsset = 'bitcoin';
+let selectedTimePeriod = '365';
+
+// GET THE PRICE AND TIME OF ASSET
+async function fetchData() {
+
+    try {
+        // link to fetch data from CoinGecko
+        let URL = `https://api.coingecko.com/api/v3/coins/${selectedAsset}/market_chart?vs_currency=usd&days=${selectedTimePeriod}`;
+
+        // Get the dataset from CoinGecko API
+        let response = await fetch(URL);
+        let data = await response.json();
+        let prices = await data.prices;
+
+        // remove the old data on the chart
+        chartTimeframe = [];
+        chartPrice = [];
+
+        // extraction of the desired data from dataset (time and price)
+        // use this instead of forEach for normal for loop for async await
+        for (const price of prices) {
+            // extract price and time values
+            let epochTimeframe = await price[0];
+            let roundedprice = Math.round(price[1]);
+
+            // change from Epoch time format to UTC
+            let formattedDate = new Date(epochTimeframe);
+            let longTimeframe = formattedDate.toUTCString();
+            let timeframe = longTimeframe.substring(4, 16);        
+
+            // add data to arrays
+            chartTimeframe.push(timeframe);
+            chartPrice.push(roundedprice);
+        }
+
+        // update the chart with new data
+        displayedChart.data.labels = chartTimeframe;
+        displayedChart.data.datasets.forEach(dataset => {
+            dataset.data = chartPrice;
+            dataset.label = `Price of ${selectedAsset}`;
+        });
+        displayedChart.update();
+    }
+    catch(error) {
+        console.log('cannot get data from coingecko...')
+        console.log(error);
+    }
+};
+fetchData();
+
+// GENERATE LIST OF ASSETS
+const assetListURL = 'https://api.coingecko.com/api/v3/coins/list';
+const assetListEl = document.querySelector('.assetList');
+const assetList = [];
+
+async function getAssetList() {
+    try {
+        // fetch the list of assets
+        let response = await fetch(assetListURL);
+        let assetListData = await response.json();
+
+        for (const asset of assetListData) {
+            let assetIDs = await asset.id;
+            const listOptions = document.createElement('option');
+            listOptions.classList.add('asset');
+            listOptions.value = await assetIDs;
+            listOptions.appendChild(document.createTextNode(assetIDs));
+            assetListEl.appendChild(listOptions);
+        }
+    }
+    catch(error) {
+        console.log(error);
+        console.log('cannot get list of assets from CoinGecko...');
+    }
+}
+getAssetList();
+
+// change the asset on the chart
+function changeAsset() {
+    const assetList = document.querySelector('.assetList');
+    selectedAsset = assetList.value;
+    fetchData();
+};
+assetListEl.addEventListener('change', changeAsset);
 
 
-
+// change the timeframe on the chart
+const selectedTimePeriodEl = document.querySelector('.timeframeList');
+function changeTimeframe() {
+    const timeframeList = document.querySelector('.timeframeList');
+    selectedTimePeriod = timeframeList.value;
+    fetchData();
+}
+selectedTimePeriodEl.addEventListener('change', changeTimeframe);
 
 
     // Code for the charting librarby ChartJS
@@ -53,15 +111,10 @@ const displayedChart = new Chart(ctx, {
       labels: [chartTimeframe],
       datasets: [{
         data: [chartPrice],
-        label: 'Price of Bitcoin',
-        borderWidth: 1
+        label: `Price of ${selectedAsset}`,
+        borderWidth: 1,
+        pointRadius: 0
       }]
     },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
+    options: {}
   });
